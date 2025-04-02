565b780ecafb38f3d8884910f4bd8fe6fa531d0d
H K S V Amarakeerthi <121732372+samudivirasha@users.noreply.github.com>
2025-04-02 21:41:04 +0530

    updates

--- lib/screen/screen_admin.dart
+++ lib/screen/screen_admin.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 class HomeAdmin extends StatefulWidget {
   const HomeAdmin({super.key});
@@ -10,10 +11,92 @@ class HomeAdmin extends StatefulWidget {
 class _HomeAdminState extends State<HomeAdmin> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(),
       body: Center(
-        child: Text('Admin Home Screen'),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                SizedBox(
+                  width: 40.w,
+                  child: Form(
+                      child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Name',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(12),
+                            ),
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration:
+                            const InputDecoration(labelText: 'Measurement'),
+                      ),
+                      DropdownButtonFormField(
+                        items: const [
+                          DropdownMenuItem(
+                            value: 'high',
+                            child: Text('High'),
+                          ),
+                          DropdownMenuItem(
+                            value: 'medium',
+                            child: Text('Medium'),
+                          ),
+                          DropdownMenuItem(
+                            value: 'low',
+                            child: Text('Low'),
+                          ),
+                        ],
+                        onChanged: (value) {},
+                        decoration:
+                            const InputDecoration(labelText: 'Priority'),
+                      ),
+                      // status dropdown
+                      DropdownButtonFormField(
+                        items: const [
+                          DropdownMenuItem(
+                            value: 'pending',
+                            child: Text('Pending'),
+                          ),
+                          DropdownMenuItem(
+                            value: 'inprogress',
+                            child: Text('In Progress'),
+                          ),
+                          DropdownMenuItem(
+                            value: 'completed',
+                            child: Text('Completed'),
+                          ),
+                          DropdownMenuItem(
+                            value: 'problem',
+                            child: Text('Deliverd'),
+                          ),
+                          DropdownMenuItem(
+                            value: 'problem',
+                            child: Text('Problem'),
+                          ),
+                        ],
+                        onChanged: (value) {},
+                        decoration: const InputDecoration(labelText: 'Status'),
+                      ),
+                      TextFormField(
+                        decoration:
+                            const InputDecoration(labelText: 'Description'),
+                      ),
+                    ],
+                  )),
+                ),
+                Container(
+                  width: 40.w,
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
