b1f4e35abc31fba7523bdd7d01f4fd097e604f86
Wrashmika <54312083+Wrashmika@users.noreply.github.com>
2025-04-03 16:24:38 +0530

    Add files via upload

---
+++ lib/authentication.dart
@@ -0,0 +1,32 @@
+import 'package:cl_fashion/screen/home.dart';
+import 'package:cl_fashion/screen/login.dart';
+import 'package:cl_fashion/service/auth_service.dart';
+import 'package:cl_fashion/test.dart';
+
+import 'package:flutter/material.dart';
+
+import 'screen/screen_admin.dart';
+
+class AuthWrapper extends StatelessWidget {
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: _auth.authStateChanges,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          final user = snapshot.data;
+          // Test();
+          if (user == null) {
+            return LoginScreen();
+          } else {
+            // print(user.email);
+            return const HomeAdmin();
+          }
+        }
+        return Scaffold(body: Center(child: CircularProgressIndicator()));
+      },
+    );
+  }
+}
